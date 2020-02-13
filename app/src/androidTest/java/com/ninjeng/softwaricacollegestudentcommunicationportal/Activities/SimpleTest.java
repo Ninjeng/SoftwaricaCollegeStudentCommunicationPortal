a053d1bd5eba742ecf618b42d5048e7352ade4fb
Ninjeng <ninzenlama21@gmail.com>
2020-02-13 20:45:31 +0545

    Adding exit buttion and Simple Instrumental testing created

---
+++ app/src/androidTest/java/com/ninjeng/softwaricacollegestudentcommunicationportal/Activities/SimpleTest.java
@@ -0,0 +1,18 @@
+package com.ninjeng.softwaricacollegestudentcommunicationportal.Activities;
+
+import android.util.Log;
+import android.view.View;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SimpleTest {
+
+
+}
---
+++ app/src/test/java/com/ninjeng/softwaricacollegestudentcommunicationportal/ConnectivityTesting.java
@@ -0,0 +1,17 @@
+package com.ninjeng.softwaricacollegestudentcommunicationportal;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import org.junit.Test;
+
+public class ConnectivityTesting {
+
+    @Test
+    public void checkConnection()
+    {
+
+
+    }
+}
